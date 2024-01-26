45306e4e464f08839a15590c6d10e924dbd5f3ab
Matty Maclean <macleanmatty@gmail.com>
2024-01-26 22:39:58 +0000

    Prioritise full image for downloads

--- posters-ui/src/services/handleDownload.js
+++ posters-ui/src/services/handleDownload.js
@@ -1,14 +1,27 @@
 // handleDownload.js 
 
 const handleDownload = (images) => {
-    images.forEach((image, index) => {
-        const link = document.createElement('a');
-        link.href = image;
-        link.download = `downloaded_image_${index}.png`; // This names the files
-        document.body.appendChild(link);
-        link.click();
-        document.body.removeChild(link);
+    if (images.length === 0) return;
+
+    // Immediately download the first image
+    downloadImage(images[0], 0);
+
+    // For the rest of the images, introduce a delay
+    const delay = 100; // milliseconds
+    images.slice(1).forEach((image, index) => {
+        setTimeout(() => {
+            downloadImage(image, index + 1);
+        }, delay * (index + 1)); // Incremental delay for each subsequent image
     });
+};
+
+const downloadImage = (image, index) => {
+    const link = document.createElement('a');
+    link.href = image;
+    link.download = `downloaded_image_${index}.png`; // Naming the file
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
 };
 
 export default handleDownload;
