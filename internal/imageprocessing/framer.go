0fb5e9f5c84667556daa6e1f8185149c2fb33e2e
MATTHEW MacLean <mrm109@cs.bham.ac.uk>
2024-01-30 23:05:27 +0000

    Anchor resized imaged on frame

--- internal/imageprocessing/framer.go
+++ internal/imageprocessing/framer.go
@@ -20,14 +20,23 @@ func Frame(framePath, posterPath, outputPath string) (string, error) {
 		return "", err
 	}
 
-	// Resize the poster to fit the frame.
-	// Replace these with the actual dimensions of the area in the frame where the poster should fit.
+	// Dimensions where the poster will fit.
 	frameWidth, frameHeight := 350, 350
-	resizedPoster := imaging.Resize(poster, frameWidth, frameHeight, imaging.Lanczos)
+
+	// Determine the scaling factor.
+	scaleWidth := float64(frameWidth) / float64(poster.Bounds().Dx())
+	scaleHeight := float64(frameHeight) / float64(poster.Bounds().Dy())
+	scaleFactor := min(scaleWidth, scaleHeight)
+
+	// Scale down the poster image while maintaining its aspect ratio.
+	newWidth := int(float64(poster.Bounds().Dx()) * scaleFactor)
+	newHeight := int(float64(poster.Bounds().Dy()) * scaleFactor)
+	resizedPoster := imaging.Resize(poster, newWidth, newHeight, imaging.Lanczos)
+
+	// Coordinates where the poster should be placed on the frame.
+	frameX, frameY := 342, 325
 
 	// Superimpose the resized poster onto the frame.
-	// Replace these with the actual coordinates where the poster should be placed on the frame.
-	frameX, frameY := 342, 325
 	result := imaging.Overlay(frame, resizedPoster, image.Pt(frameX, frameY), 1.0)
 
 	// Save the output image to outputPath
@@ -38,4 +47,12 @@ func Frame(framePath, posterPath, outputPath string) (string, error) {
 
 	// Return the output path and no error
 	return outputPath, nil
+}
+
+// min returns the smaller of x or y.
+func min(x, y float64) float64 {
+	if x < y {
+		return x
+	}
+	return y
 }
