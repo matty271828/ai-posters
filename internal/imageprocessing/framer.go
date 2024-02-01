48974e5668e3ace33e296406775e7e4f9efc0c74
MATTHEW MacLean <mrm109@cs.bham.ac.uk>
2024-02-01 22:52:40 +0000

    Center image in generated frame

--- internal/imageprocessing/framer.go
+++ internal/imageprocessing/framer.go
@@ -6,7 +6,6 @@ import (
 	"github.com/disintegration/imaging"
 )
 
-// Frame takes two image paths (frame and poster) and combines them
 func Frame(framePath, posterPath, outputPath string) (string, error) {
 	// Load frame image
 	frame, err := imaging.Open(framePath)
@@ -21,25 +20,27 @@ func Frame(framePath, posterPath, outputPath string) (string, error) {
 	}
 
 	// Dimensions where the poster will fit.
-	frameWidth, frameHeight := 350, 350
+	frameWidth, frameHeight := 350, 350 // Adjust if necessary for your frame
 
-	// Determine the scaling factor.
+	// Determine the scaling factor to maintain aspect ratio.
 	scaleWidth := float64(frameWidth) / float64(poster.Bounds().Dx())
 	scaleHeight := float64(frameHeight) / float64(poster.Bounds().Dy())
 	scaleFactor := min(scaleWidth, scaleHeight)
 
-	// Scale down the poster image while maintaining its aspect ratio.
+	// Scale down the poster image.
 	newWidth := int(float64(poster.Bounds().Dx()) * scaleFactor)
 	newHeight := int(float64(poster.Bounds().Dy()) * scaleFactor)
 	resizedPoster := imaging.Resize(poster, newWidth, newHeight, imaging.Lanczos)
 
-	// Coordinates where the poster should be placed on the frame.
-	frameX, frameY := 342, 325
+	// Calculate the center position for the poster.
+	frameCenterX, frameCenterY := 342+frameWidth/2, 325+frameHeight/2 // Adjust these base coordinates to the actual frame position
+	posterX := frameCenterX - newWidth/2
+	posterY := frameCenterY - newHeight/2
 
-	// Superimpose the resized poster onto the frame.
-	result := imaging.Overlay(frame, resizedPoster, image.Pt(frameX, frameY), 1.0)
+	// Superimpose the resized poster onto the frame at the calculated position.
+	result := imaging.Overlay(frame, resizedPoster, image.Pt(posterX, posterY), 1.0)
 
-	// Save the output image to outputPath
+	// Save the output image
 	err = imaging.Save(result, outputPath)
 	if err != nil {
 		return "", err
